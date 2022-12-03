566f13180fb7129f69421746f604dd12eeff2ac7
John Puddephatt <john@jdp.org.uk>
2022-12-03 19:54:23 +0000

    move css to separate file for hiding nav

--- resources/views/template-training.blade.php
+++ resources/views/template-training.blade.php
@@ -7,15 +7,6 @@
 @section('content')
   @while (have_posts())
     @php(the_post())
-
-    ***
-    <style>
-      #main-menu-container nav {
-        display: none;
-      }
-    </style>
-    ***
-
     <div class="flex flex-row w-full border-t border-sky">
       <div class="flex-1">
         @include('partials.page-header')
