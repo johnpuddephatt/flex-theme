18a1b53b9f5b796faf6fd8aa9923f6aa4a3f8ae9
John Puddephatt <john@jdp.org.uk>
2022-12-04 16:34:12 +0000

    page children container

--- resources/views/template-menu.blade.php
+++ resources/views/template-menu.blade.php
@@ -10,13 +10,14 @@
     <div class="flex flex-row w-full border-t border-sky"> @include('partials.page-sidebar')
       <div class="flex-1">
         @include('partials.page-header')
-        @includeFirst(['partials.content-page', 'partials.content-single'])
 
-        <div class="w-full container flex flex-col pb-16 divide-y divide-sky">
+        <div class="container my-24 flex flex-col gap-4 mx-auto xl:max-w-5xl 2xl:py-32">
           @foreach ($children as $key => $page)
             @include('partials.page-card-alternative')
           @endforeach
         </div>
+
+        @includeFirst(['partials.content-page', 'partials.content-single'])
 
         @include('partials.page-siblings')
       </div>
