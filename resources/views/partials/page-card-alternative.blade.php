e8ae7e645d6a7640c6ea113e018e2a31507bf5c8
John Puddephatt <john@jdp.org.uk>
2022-12-04 17:09:26 +0000

    styling

--- resources/views/partials/page-card-alternative.blade.php
+++ resources/views/partials/page-card-alternative.blade.php
@@ -4,15 +4,15 @@
       @if (has_post_thumbnail(isset($page->ID) ? $page->ID : '') &&
           isset(wp_get_attachment_metadata(get_post_thumbnail_id($page->ID))['sizes']['square-xs']))
         {!! get_the_post_thumbnail($page->ID, 'square-xs', [
-            'class' => 'w-36 rounded-tr-medium rounded-bl-medium max-w-none block ',
+            'class' => 'w-40 rounded-tr-medium rounded-bl-medium max-w-none block ',
         ]) !!}
       @else
-        <div class="block w-36 h-36 bg-sky rounded-tr-medium rounded-bl-medium max-w-none"></div>
+        <div class="block w-40 h-40 bg-sky rounded-tr-medium rounded-bl-medium max-w-none"></div>
       @endif
     </div>
     <div class="p-6 px-8 antialiased">
-      <h3 class="mt-2 mb-4 text-xl font-semibold lg:text-2xl text-white">{!! get_the_title($page->ID) !!}</h3>
-      <span class="inline-block mt-4 py-2 px-6 rounded-xl rounded-tr-none border-2 border-green text-sm text-white">Read
+      <h3 class="mb-3 text-xl font-semibold text-white">{!! get_the_title($page->ID) !!}</h3>
+      <span class="inline-block mt-3 py-2 px-6 rounded-xl rounded-tr-none border-2 border-green text-sm text-white">Read
         more</span>
     </div>
   </div>
