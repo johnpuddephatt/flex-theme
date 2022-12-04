e12824b4542b833e87f41b9caef8777361f2ad92
John Puddephatt <john@jdp.org.uk>
2022-12-04 16:43:24 +0000

    styling

--- resources/views/partials/page-card-alternative.blade.php
+++ resources/views/partials/page-card-alternative.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ get_permalink($page->ID) }}"
   @php(post_class(''))>
-    <div class="flex flex-row bg-navy rounded-tr-medium rounded-bl-medium overflow-hidden items-center xl:max-w-3xl mx-auto">
+    <div class="flex flex-row bg-navy rounded-tr-medium rounded-bl-medium overflow-hidden xl:max-w-3xl">
     <div
         class="mb-auto hidden lg:block relative">
         @if (has_post_thumbnail(isset($page->ID) ? $page->ID : '') &&
@@ -14,9 +14,8 @@
         @endif
   </div>
   <div class="p-6 px-8">
-    <h3 class="mt-2 mb-4 text-xl font-bold lg:text-2xl text-white">{!! get_the_title($page->ID) !!}</h3>
-    <span
-      class="font-semibold inline-block mt-4 py-2 px-6 rounded-xl rounded-tr-none border-2 border-green text-sm text-white">Read
+    <h3 class="mt-2 mb-4 text-xl font-semibold lg:text-2xl text-white">{!! get_the_title($page->ID) !!}</h3>
+    <span class="inline-block mt-4 py-2 px-6 rounded-xl rounded-tr-none border-2 border-green text-sm text-white">Read
       more</span>
   </div>
   </div>
