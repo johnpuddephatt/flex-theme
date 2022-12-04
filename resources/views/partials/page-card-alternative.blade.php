367d42988cd16570e45fc44f5ef904a596bc8c3d
John Puddephatt <john@jdp.org.uk>
2022-12-04 16:39:20 +0000

    styling

--- resources/views/partials/page-card-alternative.blade.php
+++ resources/views/partials/page-card-alternative.blade.php
@@ -1,19 +1,19 @@
 <a href="{{ get_permalink($page->ID) }}"
   @php(post_class(''))>
-    <div class="flex flex-row bg-navy rounded-tr-medium overflow-hidden items-center gap-8 lg:gap-12 xl:max-w-3xl mx-auto">
+    <div class="flex flex-row bg-navy rounded-tr-big rounded-bl-big overflow-hidden items-center xl:max-w-3xl mx-auto">
     <div
         class="mb-auto hidden lg:block relative">
         @if (has_post_thumbnail(isset($page->ID) ? $page->ID : '') &&
             isset(wp_get_attachment_metadata(get_post_thumbnail_id($page->ID))['sizes']['square-xs']))
         {!! get_the_post_thumbnail($page->ID, 'square-xs', [
-            'class' => 'w-64 rounded-tr-big rounded-bl-big max-w-none block ',
+            'class' => 'w-48 rounded-tr-big rounded-bl-big max-w-none block ',
         ]) !!}
         @else
-        <div class="block w-64 h-64 bg-sky rounded-tr-big rounded-bl-big max-w-none"></div>
+        <div class="block w-48 h-48 bg-sky rounded-tr-big rounded-bl-big max-w-none"></div>
 
         @endif
   </div>
-  <div>
+  <div class="p-8">
     <h3 class="mt-2 mb-4 text-xl font-bold lg:text-2xl text-white">{!! get_the_title($page->ID) !!}</h3>
     <span
       class="font-semibold inline-block mt-8 py-3 px-6 rounded-xl rounded-tr-none border-2 border-green text-lg text-white">Read
