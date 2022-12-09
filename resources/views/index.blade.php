869944d60492d05598c37f97704ac46f298152bf
John Puddephatt <john@jdp.org.uk>
2022-12-09 15:43:04 +0000

    remove index page search form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,18 +14,18 @@
       </x-alert>
     @endif
 
-    <form class="max-w-3xl mx-auto mb-24" role="search" action="{{ home_url('/') }}" method="get" id="searchform">
-      <input type="hidden" name="post_type" value="post" />
-      <div class="flex flex-wrap px-8 py-12 lg:flex-nowrap">
-        <input type="text" aria-label="Text to search for" name="s" placeholder="Search latest updates"
-          class="text-lg flex-1 inline-block rounded-l-lg p-4 border-2 border-blue">
-        <button type="submit"
-          class="bg-blue text-white border-2 text-base border-blue inline-block px-8 py-4 font-semibold rounded-br-lg whitespace-nowrap">
-          Search
-        </button>
+    <!-- <form class="max-w-3xl mx-auto mb-24" role="search" action="{{ home_url('/') }}" method="get" id="searchform">
+        <input type="hidden" name="post_type" value="post" />
+        <div class="flex flex-wrap px-8 py-12 lg:flex-nowrap">
+          <input type="text" aria-label="Text to search for" name="s" placeholder="Search latest updates"
+            class="text-lg flex-1 inline-block rounded-l-lg p-4 border-2 border-blue">
+          <button type="submit"
+            class="bg-blue text-white border-2 text-base border-blue inline-block px-8 py-4 font-semibold rounded-br-lg whitespace-nowrap">
+            Search
+          </button>
 
-      </div>
-    </form>
+        </div>
+      </form> -->
 
     <div class="space-y-8 xl:space-y-16 max-w-4xl mx-auto">
       @while (have_posts())
