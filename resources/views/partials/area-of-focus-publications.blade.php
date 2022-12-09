0008c3d7e6de77250397aed7587a1ef0b9df2daa
John Puddephatt <john@jdp.org.uk>
2022-12-09 15:27:59 +0000

    fix area of focus publications width

--- resources/views/partials/area-of-focus-publications.blade.php
+++ resources/views/partials/area-of-focus-publications.blade.php
@@ -1,5 +1,5 @@
 @if ($publications && count($publications))
-  <div class="container xl:max-w-5xl">
+  <div class="container xl:max-w-5xl 2xl:max-w-6xl">
     <h3 class="border-t border-sky pt-12 pb-8 text-2xl font-bold mb-4 text-navy">Related publications</h3>
     <div class="space-y-4">
       @foreach ($publications as $publication)
