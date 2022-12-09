0e42b5ca8b40ce4cbd3b02247e3218d4f399b6e1
John Puddephatt <john@jdp.org.uk>
2022-12-09 14:59:20 +0000

    date on publication block

--- resources/views/partials/publication-card.blade.php
+++ resources/views/partials/publication-card.blade.php
@@ -5,8 +5,8 @@
     <div class="py-5 px-5 lg:px-10 flex-1 flex flex-col">
       <h3 class="!lg:text-xl !my-0 !text-black mb-1 !text-lg !leading-tight font-semibold">
         {!! wp_trim_words(get_the_title(isset($publication) ? $publication->ID : null), 15) !!}</h3>
-      <span class="mt-auto text-sm">{{ get_the_date(isset($publication) ? $publication->ID : null) }}</span>
-      <!-- <p class="!my-0 max-w-lg text-navy">{!! wp_trim_words(get_the_excerpt(isset($publication) ? $publication->ID : null), 15) !!}</p> -->
+
+      <span class="!mt-auto !text-sm">{{ get_the_date(null, isset($publication) ? $publication->ID : null) }}</span>
     </div>
     @svg('icons.arrow-right')
   </a>
