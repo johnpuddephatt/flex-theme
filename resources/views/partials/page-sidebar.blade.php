d36176a4f436679200d4387bffa196af299682f3
John Puddephatt <john@jdp.org.uk>
2022-12-20 17:45:14 +0000

    update page sidebar to match new condensed publication sidebar

--- resources/views/partials/page-sidebar.blade.php
+++ resources/views/partials/page-sidebar.blade.php
@@ -7,7 +7,7 @@
         </a>
       </h2>
       @foreach ($siblings as $page)
-        <a class="@if (get_permalink() == get_permalink($page->ID)) bg-opacity-100 text-navy @else hover:bg-opacity-30 bg-opacity-0 @endif pl-0 px-6 2xl:pl-0 2xl:px-8 block py-3 text-lg bg-sky transition"
+        <a class="@if (get_permalink() == get_permalink($page->ID)) bg-opacity-100 text-navy @else hover:bg-opacity-30 bg-opacity-0 @endif pl-0 px-6 2xl:pl-0 2xl:px-8 block py-1.5 text-lg bg-sky transition"
           href="{{ get_permalink($page->ID) }}">
           <span class="pl-10 2xl:pl-12 block mx-auto mr-4 w-72">{!! get_the_title($page->ID) !!}</span>
         </a>
