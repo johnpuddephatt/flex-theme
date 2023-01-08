324067a8aef111345ba53d3ed2375c3e1aea9770
John Puddephatt <john@jdp.org.uk>
2023-01-08 21:26:24 +0000

    remove space when no children

--- resources/views/template-whatwedo.blade.php
+++ resources/views/template-whatwedo.blade.php
@@ -11,11 +11,13 @@
       <div class="flex-1">
         @include('partials.page-header')
 
-        <div class="container my-16 flex flex-col gap-6 mx-auto xl:max-w-5xl 2xl:max-w-6xl 2xl:mt-24">
-          @foreach ($children as $key => $page)
-            @include('partials.page-card-alternative')
-          @endforeach
-        </div>
+        @if (count($children))
+          <div class="container my-16 flex flex-col gap-6 mx-auto xl:max-w-5xl 2xl:max-w-6xl 2xl:mt-24">
+            @foreach ($children as $key => $page)
+              @include('partials.page-card-alternative')
+            @endforeach
+          </div>
+        @endif
 
         @includeFirst(['partials.content-page', 'partials.content-single'])
         @include('partials.page-siblings')
