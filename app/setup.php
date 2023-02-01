b58dab1492431d68c0e962c5bae6a35ee6136d9a
John Puddephatt <john@jdp.org.uk>
2023-02-01 13:47:58 +0000

    disable lzb menu

--- app/setup.php
+++ app/setup.php
@@ -152,7 +152,7 @@ add_action(
                 ),
             ]);
 
-            // add_filter("lzb/show_admin_menu", "__return_false");
+            add_filter("lzb/show_admin_menu", "__return_false");
             // add_filter("acf/settings/show_admin", "__return_false");
         });
 
