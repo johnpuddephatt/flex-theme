2dc2b78d04d61bb7ee0e5d15dc88c7952844ba56
John Puddephatt <john@jdp.org.uk>
2023-03-10 15:13:29 +0000

    avatar spacing

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,5 +1,5 @@
 @if (get_the_author_meta('ID') !== 1)
-  <div class="flex flex-row items-center py-4 lg:py-8">
+  <div class="flex flex-row items-center py-4 lg:py-12">
     {!! get_avatar(get_the_author_meta('ID'), null, null, null, [
         'class' => 'max-w-none bg-sky object-cover w-12 h-12 lg:w-24 lg:h-24 mr-2 lg:mr-4 rounded-full',
     ]) !!}
