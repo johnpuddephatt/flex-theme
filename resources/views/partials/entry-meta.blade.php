23c3ec5f5c815a319d25092e1334c401816bb276
John Puddephatt <john@jdp.org.uk>
2023-03-10 15:12:19 +0000

    larger avatars

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,7 +1,7 @@
 @if (get_the_author_meta('ID') !== 1)
   <div class="flex flex-row items-center py-4 lg:py-8">
     {!! get_avatar(get_the_author_meta('ID'), null, null, null, [
-        'class' => 'max-w-none bg-sky object-cover w-12 h-12 lg:w-16 lg:h-16 mr-2 lg:mr-4 rounded-full',
+        'class' => 'max-w-none bg-sky object-cover w-12 h-12 lg:w-24 lg:h-24 mr-2 lg:mr-4 rounded-full',
     ]) !!}
     <div class="text-blue">
       <p class="text-lg lg:text-xl mb-1 font-bold">
