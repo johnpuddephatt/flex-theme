4ac8b68a5eadb10be66b06019f9a70d80ecd7d3c
John Puddephatt <john@jdp.org.uk>
2023-03-21 13:11:19 +0000

    limit publications block to 3

--- blocks/lazyblock-tagged-publications/block.php
+++ blocks/lazyblock-tagged-publications/block.php
@@ -2,7 +2,7 @@
 <?php echo \Roots\view("partials.publications-block", [
     "publications" => get_posts([
         "post_type" => "publication",
-        "posts_per_page" => 5,
+        "posts_per_page" => 3,
         "orderby" => "date",
         "tag__in" => [$attributes["tag"]],
     ]),
